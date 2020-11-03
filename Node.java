a139e08c36ab78ae3f27a08dd6d9c14bf3ddb82d
PhilipCM <pmou@itu.dk>
2020-11-03 10:54:47 +0100

    First

---
+++ Node.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+
+public class Node{
+    public static void main(String[] args) throws IOException{
+        int port;
+        int key;
+        String value;
+        int left;
+        int right;
+
+        while (true){
+
+            //Creates threads to handle incoming connections. 
+        }
+    }
+}
